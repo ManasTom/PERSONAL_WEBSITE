97c132740f92c8501fca905774826b70151ed90f
ManasTom <manastom670@gmail.com>
2023-09-07 22:30:08 +0530

    done

---
+++ script.js
@@ -0,0 +1,11 @@
+myButton.addEventListener("click", function () {
+    myPopup.classList.add("show");
+});
+closePopup.addEventListener("click", function () {
+    myPopup.classList.remove("show");
+});
+window.addEventListener("click", function (event) {
+    if (event.target == myPopup) {
+        myPopup.classList.remove("show");
+    }
+});
